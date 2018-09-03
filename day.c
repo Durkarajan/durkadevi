b030d1247889fd3d7a192ff00ed74f5e320ff5c4
Durkarajan <36723396+Durkarajan@users.noreply.github.com>
2018-09-03 15:42:31 +0530

    Create day.c

---
+++ day.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str[50];
+printf("");
+scanf("%s",str);
+if(str[0]=='s'||str[0]=='S')
+{
+printf("Yes");
+}
+else
+{
+printf("No");
+}
+}
